05832ae962e5dfc88022d4bf4cc02a79b4254f74
WYKCpu <1074154081@qq.com>
2026-01-19 15:07:00 +0800

    scripts~~~

--- codeComplex/demo/scripts/fit_only_v4.py
+++ codeComplex/demo/scripts/fit_only_v4.py
@@ -589,13 +589,13 @@ def main():
     }
     
     folder_config = {
-        # 'constant': (config.constant_folder_path, ['Constant', 'constant']),
-        # 'logn': (config.logn_folder_path, ['Logarithmic', 'logn']),
-        # 'linear': (config.linear_folder_path, ['Linear']),
-        # 'nlogn': (config.nlogn_folder_path, ['N Log N', 'nlogn', 'n_log_n']),
-        # 'quadratic': (config.quadratic_folder_path, ['Quadratic']),
-        # 'cubic': (config.cubic_folder_path, ['Cubic']),
-        'np': (config.np_folder_path, ['NP']),
+        'constant': (config.constant_folder_path, ['Constant', 'constant']),
+        'logn': (config.logn_folder_path, ['Logarithmic', 'logn']),
+        'linear': (config.linear_folder_path, ['Linear']),
+        'nlogn': (config.nlogn_folder_path, ['N Log N', 'nlogn', 'n_log_n']),
+        'quadratic': (config.quadratic_folder_path, ['Quadratic']),
+        'cubic': (config.cubic_folder_path, ['Cubic']),
+        # 'np': (config.np_folder_path, ['NP']),
     }
     
     base_dir_map = {
