ddcfec1e32e7d984bbe0d90ba93dda54ab2c451b
WYKCpu <1074154081@qq.com>
2026-01-25 18:03:41 +0800

    data---

--- codeComplex/demo/scripts/fit_only_v4.py
+++ codeComplex/demo/scripts/fit_only_v4.py
@@ -626,11 +626,11 @@ def main():
     
     folder_config = {
         # 'constant': (config.constant_folder_path, ['Constant', 'constant']),
-        'logn': (config.logn_folder_path, ['Logarithmic', 'logn']),
-        'linear': (config.linear_folder_path, ['Linear']),
-        'nlogn': (config.nlogn_folder_path, ['N Log N', 'nlogn', 'n_log_n']),
-        'quadratic': (config.quadratic_folder_path, ['Quadratic']),
-        'cubic': (config.cubic_folder_path, ['Cubic']),
+        # 'logn': (config.logn_folder_path, ['Logarithmic', 'logn']),
+        # 'linear': (config.linear_folder_path, ['Linear']),
+        # 'nlogn': (config.nlogn_folder_path, ['N Log N', 'nlogn', 'n_log_n']),
+        # 'quadratic': (config.quadratic_folder_path, ['Quadratic']),
+        # 'cubic': (config.cubic_folder_path, ['Cubic']),
         'np': (config.np_folder_path, ['NP']),
     }
     
